d9902d4fe0d4e3157dc21851f19720bbd0b2e4ac
Auxia <sbhathal12@gmail.com>
2022-11-13 23:51:29 +0000

    Small Fixes

--- Daily Problems/1. Two Sum/TwoSum.java
+++ Daily Problems/1. Two Sum/TwoSum.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 /* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -6,19 +6,19 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order. */
 
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
+   public int[] twoSum(int[] nums, int target) {
+       int[] result = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
-        
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(map.containsKey(target-nums[i])){
-                result[0] = i;
-                result[1] = map.get(target-nums[i]);
-                return result;
-            }
-            map.put(nums[i], i);
-        }
-        return result;
-    }
+
+       for(int i = 0; i < nums.length; i++)
+       {
+           if(map.containsKey(target-nums[i])){
+               result[0] = i;
+               result[1] = map.get(target-nums[i]);
+               return result;
+           }
+           map.put(nums[i], i);
+       }
+       return result;
+   }
 }
